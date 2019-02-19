9bb68ffb83f928eaa033df19b6f7790fa1c62139
Tomasz Nocoń <thom.nocon@gmail.com>
2019-02-19 14:16:45 +0100

    Remove not-needed Data annotation from KeycloakAdapaterProperties

--- src/main/java/com/novomatic/elasticsearch/proxy/config/KeycloakAdapterProperties.java
+++ src/main/java/com/novomatic/elasticsearch/proxy/config/KeycloakAdapterProperties.java
@@ -1,11 +1,9 @@
 package com.novomatic.elasticsearch.proxy.config;
 
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.keycloak.representations.adapters.config.AdapterConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@Data
 @NoArgsConstructor
 @ConfigurationProperties(prefix = "keycloak")
 public class KeycloakAdapterProperties extends AdapterConfig {
